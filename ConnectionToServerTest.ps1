1b71f1836b18f4e4e55effe8ad465ddd43e9805a
Josh <113737161+wesopradajosh@users.noreply.github.com>
2025-06-05 17:23:01 -0700

    Add files via upload

---
+++ ConnectionToServerTest.ps1
@@ -0,0 +1,27 @@
+﻿# Placeholder servers, will make it so the script accesses something like a text file to grab the names of the endpoints
+$endpoints = @('PC1', 'PC2', 'PC3', 'SRV1')
+
+Write-Host "Testing all endpoints connectivity to DC"
+
+for ($i = 0; $i -lt $endpoints.length; $i++){
+
+    Test-Connection -ComputerName $endpoints[$i] -Count 1
+    
+    if (Test-Connection -ComputerName $endpoints[$i] -Quiet -Count 1){
+    
+       Write-Host "Server has successfully reached " $endpoints[$i] -ForegroundColor Green
+    
+    }
+
+    else 
+    
+    {
+    
+         Write-Host "Server cannot reach " $endpoints[$i] -ForegroundColor Red
+
+    }
+
+    
+
+
+}
