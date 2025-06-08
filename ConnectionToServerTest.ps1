d5be7e20699a9d4d75a931b1ab0b18598e853b0e
Josh <113737161+wesopradajosh@users.noreply.github.com>
2025-06-07 21:33:53 -0700

    Add files via upload
    Improved version

--- ConnectionToServerTest.ps1
+++ ConnectionToServerTest.ps1
@@ -1,27 +1,40 @@
-﻿# Placeholder servers, will make it so the script accesses something like a text file to grab the names of the endpoints
-$endpoints = @('PC1', 'PC2', 'PC3', 'SRV1')
+﻿# This should be changed to the folder with the names of your endpoints in your domain
+
+$endpoints = Get-Content C:\Users\Example\Desktop\Endpoints.txt
+$timeStamps = Get-Date
 
 Write-Host "Testing all endpoints connectivity to DC"
 
-for ($i = 0; $i -lt $endpoints.length; $i++){
 
-    Test-Connection -ComputerName $endpoints[$i] -Count 1
-    
-    if (Test-Connection -ComputerName $endpoints[$i] -Quiet -Count 1){
-    
-       Write-Host "Server has successfully reached " $endpoints[$i] -ForegroundColor Green
-    
-    }
 
-    else 
-    
-    {
-    
-         Write-Host "Server cannot reach " $endpoints[$i] -ForegroundColor Red
+for ($i = 0; $i -lt $endpoints.length; $i++) {
 
-    }
+        try {
+             Test-Connection -ComputerName $endpoints[$i] -Count 1 -ErrorAction Stop
+    
+                if (Test-Connection -ComputerName $endpoints[$i] -Quiet -Count 1){
+    
+                    Write-Host $timeStamps "Server has successfully reached" $endpoints[$i] -ForegroundColor Green
+    
+            }
 
     
 
+        } catch {
+
+             Write-Host $timeStamps "Failed to connect to" $endpoints[$i] -ForegroundColor Red
+
+
+        }
 
 }
+
+    
+    
+
+
+
+
+
+
+
