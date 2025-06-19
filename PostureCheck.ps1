bde813c216f8d00b254267b110acc9b1e3e1d63c
Josh <113737161+wesopradajosh@users.noreply.github.com>
2025-06-19 13:00:04 -0700

    Add files via upload

---
+++ PostureCheck.ps1
@@ -0,0 +1,33 @@
+﻿Import-Module BurntToast
+
+
+
+function Check-Posture {
+
+    param(
+    [Parameter(Mandatory)]
+    [ValidateSet(1,2)]
+    [int]$Message
+    )
+
+    if ($message -eq 1){
+        
+        New-BurntToastNotification -Text "POSTURE CHECK!!!!!!!!"
+
+    }
+    if ($Message -eq 2){
+        
+         New-BurntToastNotification -Text "ARE YOU STANDING STRAIGHT?? FIX YOUR POSTURE" 
+        
+
+    }
+
+
+
+}
+
+$number = Get-Random -Minimum 1 -Maximum 3
+
+Write-Host $number
+
+Check-Posture -Message $number
